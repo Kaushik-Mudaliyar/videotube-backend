5e272642b72ee52dbb73b323f2b61c36a4392629
Kaushik Mudaliyar <kaushikmudaliyar1484@gmail.com>
2026-02-28 16:38:35 +0530

    Add update video feature in video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -4,7 +4,7 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { uploadOnCloudinary, deleteOnCloudinary } from "../utils/cloudinary.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
@@ -86,8 +86,72 @@ const getVideoById = asyncHandler(async (req, res) => {
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
-  const { videoId } = req.params;
   //TODO: update video details like title, description, thumbnail
+
+  // videoId is takend from the req.params so i can use it to find the video in the Video model
+  // I am getting the title,description from the req.body
+  // check whether title and description both are coming in the request
+  // by using multer middleware we have access of req.file and from which i am getting the path
+  // checks local path
+  // upload the thumbnail on cloudinary and check the response
+  // delete the previous thumbnail from the cloudinary using deleteOnCloudinary
+  // use findbyIdAndUpdate to find by id and provide all the stuff which are going to update and pass new true so it will return the newly created object in the db
+  // return response
+
+  const { videoId } = req.params;
+  if (!videoId) {
+    throw new ApiError(400, "Video Id is required");
+  }
+  const { title, description } = req.body;
+  // both title and description is required
+  if (!(title && description)) {
+    throw new ApiError(401, "All fields are required");
+  }
+  // getting thumbnail using multer middleware
+  const thubmnailLocalPath = req.file?.path;
+  // if we did not get thumbnail then throw error
+  if (!thubmnailLocalPath) {
+    throw new ApiError(400, "Thumbnail file is required");
+  }
+  // if we get it upload the thumbnail file to cloudinary
+  const thumbnail = await uploadOnCloudinary(thubmnailLocalPath);
+  // if we did not get the url after uploading then we throw error
+  if (!thumbnail.url) {
+    throw new ApiError(400, "Error while uploading the thumbnail file");
+  }
+
+  // after successfully uploading delete the old image
+  const existedVideo = await Video.findById(videoId);
+  const oldThumbnailUrl = existedVideo.thumbnail;
+  const url = new URL(oldThumbnailUrl);
+  const path = url.pathname;
+  const pathArray = path.split("/");
+  const public_id = pathArray[5].split(".")[0];
+  // console.log(public_id);
+
+  const deletedResponse = await deleteOnCloudinary(String(public_id, "video"));
+  // if (deletedResponse?.result === "ok") {
+  //   console.log("Old thumbnail file deleted successfully");
+  // }
+
+  // update the new thumbnail, title and description in the db
+  const video = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: {
+        title,
+        description,
+        thumbnail: thumbnail.url,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Updated video details successfully"));
 });
 
 const deleteVideo = asyncHandler(async (req, res) => {
