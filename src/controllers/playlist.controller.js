55f007b7716f9448e4454d572c9e0073f9e9c8f7
Kaushik Mudaliyar <kaushikmudaliyar1484@gmail.com>
2026-03-02 13:17:23 +0530

    Add get user playlists controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -6,15 +6,15 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
   //TODO: create playlist
-  // get the name and description from the req.body
+  // get the name, description, and visibility from the req.body
   // get the user's id from req.user
   // check name and description
   // create playlist
-  const { name, description } = req.body;
-  if (!name.trim() || !description.trim()) {
+  const { name, description, visibility } = req.body;
+  if (!name.trim() || !description.trim() || !visibility) {
     throw new ApiError(
       400,
-      "Name and description is required while creating a playlist"
+      "Name, description and visibility of playlist is required while creating a playlist"
     );
   }
 
@@ -22,19 +22,46 @@ const createPlaylist = asyncHandler(async (req, res) => {
     name,
     description,
     owner: req.user._id,
+    visibility,
   });
 
   if (!playlist) {
     throw new ApiError(400, "Error while creating the playlist");
   }
   return res
-    .status(200)
+    .status(201)
     .json(new ApiResponse(201, playlist, "Playlist created successfully"));
 });
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
   const { userId } = req.params;
   //TODO: get user playlists
+  // check userId whether it is a validObjectId or not
+  // use find with the owner's id and get all the playlist
+  // check response
+  // return the response
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "User Id is not Valid");
+  }
+
+  let filter = { owner: userId };
+
+  // If requesting someone else's playlists
+  if (req.user._id.toString() !== userId) {
+    filter.visibility = "public";
+  }
+
+  const userPlaylist = await Playlist.find(filter);
+  if (userPlaylist.length === 0) {
+    throw new ApiError(400, "Playlist not found");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, userPlaylist, "User Playlist fetched successfully")
+    );
 });
 
 const getPlaylistById = asyncHandler(async (req, res) => {
