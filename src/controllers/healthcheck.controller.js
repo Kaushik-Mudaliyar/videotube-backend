25fe6b9d6594bdfe5accd85c50190981a3216f0d
Kaushik Mudaliyar <kaushikmudaliyar1484@gmail.com>
2026-03-04 12:27:33 +0530

    Add health check controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,13 +1,23 @@
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import mongoose from "mongoose";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
-})
+  //TODO: build a healthcheck response that simply returns the OK status as json with a message
+  const isDbConnected = mongoose.connection.readyState === 1;
 
-export {
-    healthcheck
-    }
-    
+  return res.status(isDbConnected ? 200 : 503).json(
+    new ApiResponse(
+      isDbConnected ? 200 : 503,
+      {
+        server: "running",
+        database: isDbConnected ? "connected" : "disconnected",
+        timestamp: new Date(),
+      },
+      isDbConnected ? "System is healthy" : "database disconnected"
+    )
+  );
+});
+
+export { healthcheck };
